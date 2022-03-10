9bd12e8e15905312a7917cf09e56b83ea4878e7e
Shima Keramati <shimakeramati@Shimas-MacBook-Pro.local>
2022-03-09 19:29:35 -0500

    buttons and dynamic functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/answer.dart';
+import './question.dart';
 
-void main() => runApp(myApp());
+void main() => runApp(MyApp());
 
-class myApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'what\'s your fav color?',
+      'answer': ["black", "green", "red"]
+    },
+    {
+      'questionText': 'what\'s your fav animal?',
+      'answer': ["rabbit", "khers", "gav"]
+    },
+    {
+      'questionText': 'what\'s your fav harchi?',
+      'answer': ["rabbitq", "khersqq", "gavq"]
+    }
+  ];
+  var _questionIndex = 0;
+
+  void _naswerQuestions() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+
+    print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print("hi");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +44,17 @@ class myApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Shimaaaaa is awesome!'),
           ),
-          body: Text('Dart and flutter is awesome!')),
+          body: Column(
+            children: [
+              Question(
+                questions[_questionIndex]['questionText'],
+              ),
+              ...(questions[_questionIndex]['answer'] as List<String>)
+                  .map((answer) {
+                return Answer(_naswerQuestions, answer);
+              }).toList()
+            ],
+          )),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,6 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:flutter_complete_guide/main.dart';
 
 void main() {
