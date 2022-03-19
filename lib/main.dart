7d8eafd37dde1aa5282ba0d4e2e29306829eaa20
Shima93 <shimakeramatim@gmail.com>
2022-03-19 12:34:42 -0400

    test

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Flutter Apps',
       home: MyHomePage(),
     );
   }
